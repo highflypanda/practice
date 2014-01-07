ddc0a80f2dcbb3d35d1f455f2735ec2f7171d484
highflypanda <leungsauyingsandy@sandy2.local>
2014-01-08 01:00:22 +0800

    Testing

---
+++ TestCase.java
@@ -0,0 +1,25 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestCase {
+	
+	Equi ep = new Equi();
+	
+	@Test
+	public void test() {
+
+	}
+	
+	@Test
+	public void test1() {
+		ep.solution3();
+		//ep.solution();
+	}
+	
+	@Test
+	public void test2() {
+		ep.solution4();
+		//ep.solution();
+	}
+}
